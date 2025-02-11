7b7a1093c2fd6df251213054f3d43c23a8ec81d7
Max-Ritter-QA <maxritterr@gmail.com>
2025-02-11 21:21:47 +0300

    HW

---
+++ 05_lesson/Mozila.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/entry_ad")
+
+button_1 = driver.find_element(By.CSS_SELECTOR, "div.modal-footer")
+button_1.click()
+
