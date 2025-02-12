1c4335e40d179f8d301b515b6c0cbc1cf1c07f26
Max-Ritter-QA <maxritterr@gmail.com>
2025-02-12 11:42:51 +0300

    HW

--- 05_lesson/Mozila.py
+++ 05_lesson/Mozila.py
@@ -1,8 +1,10 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
 driver.get("http://the-internet.herokuapp.com/entry_ad")
+sleep(3)
 
 button_1 = driver.find_element(By.CSS_SELECTOR, "div.modal-footer")
 button_1.click()
